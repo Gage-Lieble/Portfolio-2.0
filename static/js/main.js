0366680bee2182b5e88ff18e0eaa6f47a41b4db0
Gage Lieble <gagelieble@hotmail.com>
2022-08-23 00:35:11 -0700

    Nav Fixed, 1 link, Scroll on click

--- static/js/main.js
+++ static/js/main.js
@@ -2,25 +2,40 @@
 // NAV
 let navToggle = document.querySelector('.toggle')
 let navOptions = document.getElementById('nav-option-wrap')
+let navBtns = document.querySelectorAll('.nav-option')
 navToggle.addEventListener('click', () => {
     navToggle.classList.toggle('active')
     navOptions.classList.toggle('fade')
-    // if (navOptions.style.display == 'none'){
-    //     navOptions.style.display = 'block'
-        
-    // }else{
-        
-    //     setTimeout(() => {navOptions.style.display = 'none'}, 250)
-        
-    // }
-    
+    setTimeout(() => {
+        navOptions.classList.toggle('closed')
+    }, 275)
 })
+        // Scroll Animations
+
+let whoAmIBtn = document.getElementById('nav-about')
+let whoAmISec = document.getElementById('about-wrapper')
+
+whoAmIBtn.addEventListener('click', function(){
+    navToggle.classList.toggle('active')
+    navOptions.classList.toggle('fade')
+    setTimeout(() => {
+        navOptions.classList.toggle('closed')
+    }, 275)
+    
+    
+    whoAmISec.scrollIntoView({behavior: 'smooth'})
+})
+
+
+
+
+
 
 
 // MAIN WORD CHANGER
 let mainWord = document.getElementById('main-word')
 let start = 0
-let randomWords = ['SOFTWARE', 'WEBSITE', 'DJANGO']
+let randomWords = ['SOFTWARE','DJANGO' , 'WEBSITE', 'PYTHON', ]
 
 function changeWord(){
     mainWord.innerText = randomWords[start]
